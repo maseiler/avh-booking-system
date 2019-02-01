ec87ae46e22aee350c46d40a26eaa2a583ee9e29
maseiler <matthiasseiler@outlook.com>
2019-02-01 14:24:24 +0100

    Clear inputs of AddUserForm

--- client/src/components/AddUserForm.js
+++ client/src/components/AddUserForm.js
@@ -16,16 +16,26 @@ export default {
         this.$http.post('/newUser', this.newUser).then(function (response) {
           // Success
           console.log("Added new user:", this.newUser.bierName, this.newUser.firstName, this.newUser.lastName, this.newUser.email, this.newUser.phone, this.newUser.status)
+          // TODO: visible confirmation
+          this.resetAndCloseForm()
         },function (response) {
             // Error
             console.log(response.data)
+            this.resetAndCloseForm()
         });
-        // TODO empty fields
       },
       cancelSubmission(){
-        // TODO empty fields
-        this.$emit("close");
         console.log("canceled submission")
-      }
+        this.resetAndCloseForm()
+      },
+      resetAndCloseForm(){
+        this.bierName = '',
+        this.firstName = '',
+        this.lastName = '',
+        this.email ='',
+        this.phone = '',
+        this.status = ''
+        this.$emit("close");
+      },
     }
   };
