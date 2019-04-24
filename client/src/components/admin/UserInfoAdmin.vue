36d0870732aac286910c10a054b856c5c5fe7543
maseiler <matthiasseiler@outlook.com>
2019-04-24 20:46:10 +0200

    Visual changes to UserSettings. Added UserInfoAdmin.

---
+++ client/src/components/admin/UserInfoAdmin.vue
@@ -0,0 +1,63 @@
+<template>
+  <article class="message is-link" v-if="Object.keys(user).length !== 0">
+    <div class="message-header">
+      <p>{{displayUserName(user)}}</p>
+    </div>
+    <div class="message-body">
+      <div class="columns">
+        <div class="column has-text-left">
+          <h6 class="subtitle is-6">
+            <div>
+              <font-awesome-icon icon="sort-numeric-down"/>
+              &nbsp;{{user.UserID}}
+            </div>
+            <div>
+              <font-awesome-icon icon="beer"/>
+              &nbsp;{{user.BierName}}
+            </div>
+            <div>
+              <font-awesome-icon icon="user"/>
+              &nbsp;{{user.FirstName}}
+            </div>
+            <div>
+              <font-awesome-icon icon="user-circle"/>
+              &nbsp;{{user.LastName}}
+            </div>
+            <div>
+              <font-awesome-icon icon="info-circle"/>
+              &nbsp;{{user.Status}}
+            </div>
+          </h6>
+        </div>
+        <div class="column has-text-left">
+          <h6 class="subtitle is-6">
+            <div>
+              <font-awesome-icon icon="envelope"/>
+              &nbsp;{{user.Email}}
+            </div>
+            <div>
+              <font-awesome-icon icon="phone"/>
+              &nbsp;{{user.Phone}}
+            </div>
+            <div>
+              &nbsp;<font-awesome-icon icon="euro-sign"/>
+              &nbsp;&nbsp;{{user.Balance}}&nbsp;€
+            </div>
+            <div>
+              <font-awesome-icon icon="credit-card"/>
+              &nbsp;{{user.MaxDebt}}&nbsp;€
+            </div>
+          </h6>
+        </div>
+      </div>
+    </div>
+  </article>
+</template>
+
+<script>
+export default {
+  props: {
+    user: Object
+  }
+};
+</script>
--- client/src/components/admin/UserSettings.vue
+++ client/src/components/admin/UserSettings.vue
@@ -1,40 +1,13 @@
 <template>
   <div>
-    <h1>USER SETTINGS</h1>
+    <br>
     <div class="columns">
-      <div class="column is-4">
-        <div class="columns">
-          <div class="column">
-            <div class="field">
-              <div class="control has-icons-left">
-                <input
-                  class="input"
-                  type="text"
-                  placeholder="Search user"
-                  v-model="search"
-                  v-on:keyup="searchUsers"
-                >
-                <span class="icon is-small is-left">
-                  <font-awesome-icon icon="search"/>
-                </span>
-              </div>
-            </div>
-          </div>
+      <div class="column is-1">
+        <div class="buttons">
+          <button class="button is-link is-fullwidth" @click="showAddUserFormAdmin = true">Add User</button>
+          <button class="button is-link is-fullwidth" @click="showModifyUserForm = true">Modify User</button>
+          <button class="button is-link is-fullwidth" @click="showDeleteUserForm = true">Delete User</button>
         </div>
-        <div class="buttons" v-if="searchResults != []">
-          <button
-            class="button"
-            v-for="user in searchResults"
-            :key="user"
-            @click="selectedUser = user"
-            :class="[selectedUser === user ? 'is-link' : '']"
-          >{{ displayUserName(user) }}</button>
-        </div>
-      </div>
-      <div class="column is-narrow">
-        <button class="button is-link" @click="showAddUserFormAdmin = true">Add User</button>
-        <button class="button is-link" @click="showModifyUserForm = true">Modify User</button>
-        <button class="button is-link" @click="showDeleteUserForm = true">Delete User</button>
         <AddUserFormAdmin v-if="showAddUserFormAdmin" @close="showAddUserFormAdmin = false"/>
         <ModifyUserForm
           :user="selectedUser"
@@ -46,7 +19,12 @@
           v-if="showDeleteUserForm"
           @close="showDeleteUserForm = false"
         />
-        <UserInfo :user="selectedUser"/>
+      </div>
+      <div class="column is-4">
+        <UserSearch :allUsers="users" @selectUser="selectUser"/>
+      </div>
+      <div class="column is-4">
+        <UserInfoAdmin :user="selectedUser"/>
       </div>
     </div>
   </div>
@@ -56,14 +34,16 @@
 import AddUserFormAdmin from "./AddUserFormAdmin.vue";
 import ModifyUserForm from "./ModifyUserForm.vue";
 import DeleteUserForm from "./DeleteUserForm.vue";
-import UserInfo from "./UserInfo.vue";
+import UserSearch from "../booking/UserSearch.vue";
+import UserInfoAdmin from "./UserInfoAdmin.vue";
 
 export default {
   components: {
     AddUserFormAdmin,
     ModifyUserForm,
     DeleteUserForm,
-    UserInfo
+    UserSearch,
+    UserInfoAdmin
   },
   props: {
     users: []
@@ -73,24 +53,13 @@ export default {
       showAddUserFormAdmin: false,
       showModifyUserForm: false,
       showDeleteUserForm: false,
-      search: "",
-      searchResults: [],
       selectedUser: {}
     };
   },
   methods: {
-    searchUsers: function() {
-      if (this.search != "") {
-        var tmpSearch = this.search.toLowerCase();
-        this.searchResults = this.users.filter(
-          user =>
-            user["BierName"].toLowerCase().includes(tmpSearch) |
-            user["FirstName"].toLowerCase().includes(tmpSearch) |
-            user["LastName"].toLowerCase().includes(tmpSearch)
-        );
-      } else {
-        this.searchResults = [];
-      }
+    selectUser: function(user) {
+      this.selectedUser = user;
+      this.$emit("selectedUser", user);
     }
   }
 };
