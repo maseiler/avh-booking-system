4f9f37d57ceae47d575456fb62fb1926342e72ee
Matthias Seiler <39630142+maseiler@users.noreply.github.com>
2019-06-23 11:52:36 +0200

    Implemented password change

--- client/src/components/admin/OtherSettings.vue
+++ client/src/components/admin/OtherSettings.vue
@@ -1,5 +1,32 @@
 <template>
-    <div>
-        <h1>OTHER SETTINGS</h1>
+  <div>
+    <br>
+    <div class="columns">
+      <div class="column is-2">
+        <div class="buttons">
+          <button
+            class="button is-link is-fullwidth"
+            @click="showChangePasswordForm = true"
+          >Change Password</button>
+        </div>
+      </div>
+      <div class="column is-3">
+        <ChangePasswordForm v-if="showChangePasswordForm"/>
+      </div>
     </div>
+  </div>
 </template>
+
+<script>
+import ChangePasswordForm from "./ChangePasswordForm.vue";
+export default {
+  components: {
+    ChangePasswordForm
+  },
+  data() {
+    return {
+      showChangePasswordForm: false
+    };
+  }
+};
+</script>
