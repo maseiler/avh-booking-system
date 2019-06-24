b9e7d7aa7097ffe86bf69f813379208debba231e
maseiler <matthiasseiler@outlook.com>
2019-06-24 14:37:05 +0200

    WIP

--- client/src/components/statistics/Statistics.vue
+++ client/src/components/statistics/Statistics.vue
@@ -1,3 +1,19 @@
 <template>
-    <h1>Statistics</h1>
+  <div>
+    <br>
+    <img class="logo" src="~@/assets/WIP.png">
+    <br>
+    {{users[0].FirstName}}
+    {{users[0].BierName}}
+  </div>
 </template>
+
+<script>
+export default {
+  computed: {
+    users() {
+      return this.$store.state.storeUsers;
+    }
+  }
+};
+</script>
--- client/src/main.js
+++ client/src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import VueRouter from 'vue-router'
 import VueResource from 'vue-resource'
+import Vuex from 'vuex'
 import Home from './components/Home.vue'
 import Booking from './components/booking/Booking.vue'
 import Login from './components/admin/Login.vue'
@@ -82,11 +83,56 @@ const mixin = Vue.mixin({
   }
 })
 
+// TODO refacore in own file
+Vue.use(Vuex)
+
+function sortByName(array) {
+  array.sort(function(a, b) {
+    var bierNameA = a.BierName.toLowerCase(),
+      bierNameB = b.BierName.toLowerCase();
+    var firstNameA = a.FirstName.toLowerCase(),
+      firstNameB = b.FirstName.toLowerCase();
+    var lastNameA = a.LastName.toLowerCase(),
+      lastNameB = b.LastName.toLowerCase();
+    if (bierNameA < bierNameB) return -1;
+    if (bierNameA > bierNameB) return 1;
+    if (firstNameA < firstNameB) return -1;
+    if (firstNameA > firstNameB) return 1;
+    if (lastNameA < lastNameB) return -1;
+    if (lastNameA > lastNameB) return 1;
+    return 0;
+  });
+}
+
+const store = new Vuex.Store({
+  state: {
+    storeUsers: []
+  },
+  mutations: {
+    getUsers(state) {
+      console.log("called getUsers in store")
+      var users
+      Vue.http.get("/getUsers").then(response => {
+        users = [].concat.apply([], response.body);
+        sortByName(users);
+        console.log(users)
+        state.storeUsers = users
+        console.log(state.storeUsers)
+      })
+      // console.log(users)
+      // state.storeUsers = users
+    }
+  },
+})
+
+store.commit("getUsers");
+
 new Vue({
   render: h => h(App),
   // el: '#app',
   // template: '<App/>',
   // components: {App, Booking},
+  store,
   router,
   mixin
 }).$mount('#app')
