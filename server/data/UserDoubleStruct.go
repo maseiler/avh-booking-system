18ddbe5e04e1a5d5849202149456e501da74c5f5
maseiler <matthiasseiler@outlook.com>
2020-02-14 14:10:48 +0100

    Fixed typos

--- server/data/UserDoubleStruct.go
+++ server/data/UserDoubleStruct.go
@@ -1,7 +1,7 @@
 package data
 
 // UserDouble represents user and double value
-// DoubleValue is most likely balance part to be payed
+// DoubleValue is most likely balance part to be paid
 type UserDouble struct {
 	User        User
 	DoubleValue float32
