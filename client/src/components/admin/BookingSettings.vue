e9af9339be6ea49084c344645cefd56b00dacd2d
Matthias Seiler <matthiasseiler@outlook.com>
2020-10-11 13:49:19 +0200

    Implementd csv export for bookings

--- client/src/components/admin/BookingSettings.vue
+++ client/src/components/admin/BookingSettings.vue
@@ -4,36 +4,68 @@
     <div class="columns">
       <div class="column is-2">
         <br />
-        <button class="button is-link is-fullwidth is-outlined" @click="getBookings">Get Bookings</button>
+        <button
+          class="button is-link is-fullwidth is-outlined"
+          @click="getBookings"
+        >
+          Get Bookings
+        </button>
         <br />
-        <button class="button is-link is-fullwidth" @click="option = 'optLastN'">Last N</button>
+        <button
+          class="button is-link is-fullwidth"
+          @click="option = 'optLastN'"
+        >
+          Last N
+        </button>
         <div class="box" v-if="option === 'optLastN'">
           <br />
           <div class="field">
             <div class="control">
-              <input class="input is-info" type="number" placeholder="N" v-model="n" />
+              <input
+                class="input is-info"
+                type="number"
+                placeholder="N"
+                v-model="n"
+              />
             </div>
           </div>
         </div>
         <br />
 
-        <button class="button is-link is-fullwidth" @click="option = 'optFromUser'">Of User</button>
+        <button
+          class="button is-link is-fullwidth"
+          @click="option = 'optFromUser'"
+        >
+          Of User
+        </button>
         <div class="box" v-if="option === 'optFromUser'">
           <br />
           <UserSearch />
-          <p class="p has-text-grey-light">Time span can be left empty or in format YYYY-MM-DD</p>
+          <p class="p has-text-grey-light">
+            Time span can be left empty or in format YYYY-MM-DD
+          </p>
           <div class="columns">
             <div class="column">
               <div class="field">
                 <div class="control">
-                  <input class="input is-info" type="Text" placeholder="From" v-model="from" />
+                  <input
+                    class="input is-info"
+                    type="Text"
+                    placeholder="From"
+                    v-model="from"
+                  />
                 </div>
               </div>
             </div>
             <div class="column">
               <div class="field">
                 <div class="control">
-                  <input class="input is-info" type="Text" placeholder="To" v-model="to" />
+                  <input
+                    class="input is-info"
+                    type="Text"
+                    placeholder="To"
+                    v-model="to"
+                  />
                 </div>
               </div>
             </div>
@@ -41,23 +73,40 @@
         </div>
         <br />
 
-        <button class="button is-link is-fullwidth" @click="option = 'optFromItem'">Of Item</button>
+        <button
+          class="button is-link is-fullwidth"
+          @click="option = 'optFromItem'"
+        >
+          Of Item
+        </button>
         <div class="box" v-if="option === 'optFromItem'">
           <br />
           <ItemSearch :mode="'single'" />
-          <p class="p has-text-grey-light">Time span can be left empty or in format YYYY-MM-DD</p>
+          <p class="p has-text-grey-light">
+            Time span can be left empty or in format YYYY-MM-DD
+          </p>
           <div class="columns">
             <div class="column">
               <div class="field">
                 <div class="control">
-                  <input class="input is-info" type="Text" placeholder="From" v-model="from" />
+                  <input
+                    class="input is-info"
+                    type="Text"
+                    placeholder="From"
+                    v-model="from"
+                  />
                 </div>
               </div>
             </div>
             <div class="column">
               <div class="field">
                 <div class="control">
-                  <input class="input is-info" type="Text" placeholder="To" v-model="to" />
+                  <input
+                    class="input is-info"
+                    type="Text"
+                    placeholder="To"
+                    v-model="to"
+                  />
                 </div>
               </div>
             </div>
@@ -68,7 +117,9 @@
         <button
           class="button is-link is-fullwidth"
           @click="option = 'optUserPayments'"
-        >User Payments</button>
+        >
+          User Payments
+        </button>
         <div class="box" v-if="option === 'optUserPayments'">
           <br />
           <UserSearch />
@@ -80,35 +131,56 @@
             <div class="column">
               <div class="field">
                 <div class="control">
-                  <input class="input is-info" type="Text" placeholder="From" v-model="from" />
+                  <input
+                    class="input is-info"
+                    type="Text"
+                    placeholder="From"
+                    v-model="from"
+                  />
                 </div>
               </div>
             </div>
             <div class="column">
               <div class="field">
                 <div class="control">
-                  <input class="input is-info" type="Text" placeholder="To" v-model="to" />
+                  <input
+                    class="input is-info"
+                    type="Text"
+                    placeholder="To"
+                    v-model="to"
+                  />
                 </div>
               </div>
             </div>
           </div>
         </div>
         <br />
+        <button
+          class="button is-link is-fullwidth is-outlined"
+          v-on:click="downloadCsv"
+        >
+          Download
+        </button>
+        <br />
         <hr />
         <br />
-        <button class="button is-danger is-outlined" @click="undoEntry">Undo Entry</button>
-        <p
-          class="p has-text-grey-light"
-        >Modifies user's balance accordingly and creates a new book entry.</p>
+        <button class="button is-danger is-outlined" @click="undoEntry">
+          Undo Entry
+        </button>
+        <p class="p has-text-grey-light">
+          Modifies user's balance accordingly and creates a new book entry.
+        </p>
         <hr />
-        <button class="button is-danger is-outlined" @click="deleteEntry">Delete Entry</button>
+        <button class="button is-danger is-outlined" @click="deleteEntry">
+          Delete Entry
+        </button>
         <p class="p has-text-grey-light">
           Use deliberately!
           <br />The entry will be deleted and can not be restored.
         </p>
       </div>
       <div class="column">
-        <div class="box" style="height:88vh; overflow:auto;">
+        <div class="box" style="height: 88vh; overflow: auto">
           <LastBookings @selectEntry="selectEntry" :bookings="bookings" />
         </div>
       </div>
@@ -125,7 +197,7 @@ export default {
   components: {
     LastBookings,
     UserSearch,
-    ItemSearch
+    ItemSearch,
   },
   computed: {
     user() {
@@ -133,7 +205,7 @@ export default {
     },
     item() {
       return this.$store.state.selectedSingleItem;
-    }
+    },
   },
   data() {
     return {
@@ -142,7 +214,7 @@ export default {
       option: "",
       n: 0,
       from: "",
-      to: ""
+      to: "",
     };
   },
   methods: {
@@ -151,7 +223,7 @@ export default {
         if (this.n > 0) {
           this.$http
             .post("getLastNBookings", this.n)
-            .then(response => {
+            .then((response) => {
               this.bookings = [].concat.apply([], response.body);
             })
             .catch(() => {
@@ -168,7 +240,7 @@ export default {
         userFromTo.To = this.to;
         this.$http
           .post("getBookingsFromUserBetween", userFromTo)
-          .then(response => {
+          .then((response) => {
             this.bookings = [].concat.apply([], response.body);
           })
           .catch(() => {
@@ -184,7 +256,7 @@ export default {
         itemFromTo.To = this.to;
         this.$http
           .post("getBookingsFromItemBetween", itemFromTo)
-          .then(response => {
+          .then((response) => {
             this.bookings = [].concat.apply([], response.body);
           })
           .catch(() => {
@@ -200,7 +272,7 @@ export default {
         userFromTo.To = this.to;
         this.$http
           .post("getPaymentsOfUser", userFromTo)
-          .then(response => {
+          .then((response) => {
             this.bookings = [].concat.apply([], response.body);
           })
           .catch(() => {
@@ -263,7 +335,48 @@ export default {
           "Select an book entry first!"
         );
       }
-    }
-  }
+    },
+    downloadCsv() {
+      let csv = "ID,Time Stamp,User,Item,Amount,Price,Comment,Payment Method\n";
+      this.bookings.forEach((entry) => {
+        csv += entry.ID + ",";
+        csv += this.printDateTime(entry.TimeStamp) + ",";
+        csv +=
+          this.displayUserName(this.getUserByID(entry.UserID)) +
+          " (ID: " +
+          entry.UserID +
+          "),";
+        csv +=
+          this.displayItem(this.getItemByID(entry.ItemID)) +
+          " (ID: " +
+          entry.ItemID +
+          "),";
+        csv += entry.Amount + ",";
+        csv += entry.TotalPrice + ",";
+        csv += entry.Comment + ",";
+        csv += entry.PaymentMethod + "\n";
+      });
+      let fileName =
+        "bookings_" +
+        this.cleanTimeStamp(this.bookings[0].TimeStamp) +
+        "_-_" +
+        this.cleanTimeStamp(this.bookings[this.bookings.length - 1].TimeStamp) +
+        ".csv";
+
+      let anchor = document.createElement("a");
+      anchor.href = "data:text/csv;charset=utf-8," + encodeURIComponent(csv);
+      anchor.target = "_blank";
+      anchor.download = fileName;
+      anchor.click();
+    },
+    cleanTimeStamp(timeStamp) {
+      let t = timeStamp;
+      t = t.replace("T", "_");
+      t = t.replace(":", "-");
+      t = t.replace(":", "-");
+      t = t.replace("Z", "");
+      return t;
+    },
+  },
 };
 </script>
