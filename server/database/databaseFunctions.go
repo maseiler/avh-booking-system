edbd68548a6ca3c7b81c7f508a8c90597eee7246
Matthias Seiler <matthiasseiler@outlook.com>
2022-08-19 15:36:22 +0200

    Update db schema. Add constraints and triggers.

--- server/database/databaseFunctions.go
+++ server/database/databaseFunctions.go
@@ -8,7 +8,7 @@ import (
 
 var db *sql.DB
 
-// CreateDatabase creates database and all necessary tables
+// CreateDatabase creates database and all necessary tables, constraints, triggers and values
 func CreateDatabase() {
 	loginInfo := fmt.Sprintf("%s:%s@tcp(%s:%s)/", os.Getenv("AVHBS_DB_USER"), os.Getenv("AVHBS_DB_PASS"), os.Getenv("AVHBS_DB_IP"), os.Getenv("AVHBS_DB_PORT"))
 	fmt.Println("Database Login Info:")
@@ -26,6 +26,7 @@ func CreateDatabase() {
 	db, err = sql.Open("mysql", loginInfo)
 	HandleDatabaseError(err)
 
+	// create tables with constraints
 	createUsersTable := `
 	CREATE TABLE IF NOT EXISTS users(
 		id INT NOT NULL AUTO_INCREMENT,
@@ -39,7 +40,7 @@ func CreateDatabase() {
 		email VARCHAR(50),
 		phone VARCHAR(30),
 		balance DECIMAL(6,2) NOT NULL,
-		max_debt INT NOT NULL,
+		max_debt INT NOT NULL CHECK (max_debt < 0),
 		PRIMARY KEY (id)
 	);`
 	_, err = db.Exec(createUsersTable)
@@ -52,20 +53,19 @@ func CreateDatabase() {
 			client VARCHAR(20) NOT NULL,
 			name VARCHAR(50) NOT NULL,
 			type VARCHAR(20) NOT NULL,
-			size DECIMAL(6,2) NOT NULL,
+			size DECIMAL(6,2) NOT NULL CHECK (size > 0),
 			unit VARCHAR(10) NOT NULL,
-			price DECIMAL(6,2) NOT NULL,
+			price DECIMAL(6,2) NOT NULL CHECK (price > 0),
 			PRIMARY KEY (id)
 		);`
 	_, err = db.Exec(createItemsTable)
 	HandleDatabaseError(err)
 
-	// TODO create trigger: increment, if entry in bookings
 	createFavoriteItemsTable := `
 		CREATE TABLE IF NOT EXISTS favorite_items (
 			user_id INT NOT NULL,
 			item_id INT NOT NULL,
-			count INT NOT NULL,
+			count INT NOT NULL CHECK (count >= 0),
 			PRIMARY KEY (user_id, item_id)
 		);`
 	_, err = db.Exec(createFavoriteItemsTable)
@@ -78,13 +78,26 @@ func CreateDatabase() {
 			client VARCHAR(20) NOT NULL,
 			user_id INT NOT NULL,
 			item_id INT NOT NULL,
-			amount INT NOT NULL,
-			price DECIMAL(6,2) NOT NULL,
-			comment VARCHAR(255) NOT NULL,
-			payment_method VARCHAR(10) NOT NULL,
+			amount INT NOT NULL CHECK (amount > 0),
+			price DECIMAL(6,2) NOT NULL CHECK (price < 0),
+			comment VARCHAR(255),
 			PRIMARY KEY (id)
 		);`
 	_, err = db.Exec(createBookingsTable)
+	HandleDatabaseError(err)
+
+	createPaymentsTable := `
+		CREATE TABLE IF NOT EXISTS payments(
+			id INT NOT NULL AUTO_INCREMENT,
+			time_stamp DATETIME NOT NULL,
+			client VARCHAR(20) NOT NULL,
+			user_id INT NOT NULL,
+			amount INT NOT NULL CHECK (amount > 0),
+			payment_method VARCHAR(10) NOT NULL,
+			comment VARCHAR(255),
+			PRIMARY KEY (id)
+		);`
+	_, err = db.Exec(createPaymentsTable)
 	HandleDatabaseError(err)
 
 	createFeedbackTable := `
@@ -123,42 +136,70 @@ func CreateDatabase() {
 	HandleDatabaseError(err)
 	_, err = db.Exec("ALTER TABLE items ADD CONSTRAINT FOREIGN KEY (client) REFERENCES clients(name);")
 	HandleDatabaseError(err)
+	_, err = db.Exec("ALTER TABLE favorite_items ADD CONSTRAINT FOREIGN KEY (user_id) REFERENCES users(id);")
+	HandleDatabaseError(err)
+	_, err = db.Exec("ALTER TABLE favorite_items ADD CONSTRAINT FOREIGN KEY (item_id) REFERENCES items(id);")
+	HandleDatabaseError(err)
 	_, err = db.Exec("ALTER TABLE bookings ADD CONSTRAINT FOREIGN KEY (client) REFERENCES clients(name);")
+	HandleDatabaseError(err)
+	_, err = db.Exec("ALTER TABLE bookings ADD CONSTRAINT FOREIGN KEY (user_id) REFERENCES users(id);")
+	HandleDatabaseError(err)
+	_, err = db.Exec("ALTER TABLE bookings ADD CONSTRAINT FOREIGN KEY (item_id) REFERENCES items(id);")
+	HandleDatabaseError(err)
+	_, err = db.Exec("ALTER TABLE payments ADD CONSTRAINT FOREIGN KEY (client) REFERENCES clients(name);")
+	HandleDatabaseError(err)
+	_, err = db.Exec("ALTER TABLE payments ADD CONSTRAINT FOREIGN KEY (user_id) REFERENCES users(id);")
 	HandleDatabaseError(err)
 	_, err = db.Exec("ALTER TABLE feedback ADD CONSTRAINT FOREIGN KEY (client) REFERENCES clients(name);")
 	HandleDatabaseError(err)
 	_, err = db.Exec("ALTER TABLE passwords ADD CONSTRAINT FOREIGN KEY (client) REFERENCES clients(name);")
 	HandleDatabaseError(err)
 
+	// add triggers
+	incrementFavoriteItemTrigger := `
+	CREATE TRIGGER increment_favorite_item
+	AFTER INSERT ON bookings
+	FOR EACH ROW
+	BEGIN
+		IF (NEW.item_id > 0) THEN
+			IF (SELECT EXISTS(SELECT * FROM favorite_items WHERE favorite_items.user_id=NEW.user_id AND favorite_items.item_id=NEW.item_id)) = 1 THEN
+				UPDATE favorite_items SET favorite_items.count = favorite_items.count + NEW.amount WHERE favorite_items.user_id = NEW.user_id AND favorite_items.item_id = NEW.item_id;
+			ELSE
+				INSERT INTO favorite_items VALUES(NEW.user_id, NEW.item_id, NEW.amount);
+			END IF;
+		END IF;
+	END;
+	`
+	_, err = db.Exec(incrementFavoriteItemTrigger)
+	HandleDatabaseError(err)
+
+	updateBalanceTrigger := `
+	CREATE TRIGGER update_balance
+	AFTER INSERT ON payments
+	FOR EACH ROW
+	BEGIN
+		UPDATE users SET users.balance = users.balance + NEW.amount WHERE users.id = NEW.user_id;
+	END;
+	`
+	_, err = db.Exec(updateBalanceTrigger)
+	HandleDatabaseError(err)
+
 	// insert values
 	_, err = db.Exec("INSERT IGNORE INTO clients VALUES('Server', NOW());")
 	HandleDatabaseError(err)
 
-	if !passwordExists(db) {
-		_, err = db.Exec("INSERT IGNORE INTO passwords VALUES('admin', NOW(), 'Server');")
-		HandleDatabaseError(err)
-	}
+	insertPasswordIfNotExists := `
+	INSERT INTO passwords (password, creation, client)
+	SELECT 'admin', NOW(), 'Server'
+	WHERE NOT EXISTS (SELECT * FROM passwords);
+	`
+	_, err = db.Exec(insertPasswordIfNotExists)
+	HandleDatabaseError(err)
 
+	// reconnect
 	var version string
 	db.QueryRow("SELECT VERSION()").Scan(&version)
 	newLoginInfo := fmt.Sprintf("%s:***@tcp(%s:%s)/%s", os.Getenv("AVHBS_DB_USER"), os.Getenv("AVHBS_DB_IP"), os.Getenv("AVHBS_DB_PORT"), os.Getenv("AVHBS_DB_NAME"))
 	fmt.Println("AVHBS_DB_NAME:", os.Getenv("AVHBS_DB_NAME"))
 	fmt.Printf("Database set up complete: %s\n-> %s\n", version, newLoginInfo)
-}
-
-func passwordExists(db *sql.DB) bool {
-	var pws []string
-	rows, err := db.Query("SELECT password FROM passwords;")
-	HandleDatabaseError(err)
-	defer rows.Close()
-	if rows.Next() {
-		var pw string
-		err := rows.Scan(&pw)
-		pws = append(pws, pw)
-		HandleDatabaseError(err)
-	}
-	if len(pws) == 1 {
-		return true
-	}
-	return false
 }
--- server/scripts/fillDatabase.go
+++ server/scripts/fillDatabase.go
@@ -14,7 +14,6 @@ func main() {
 	loginInfo := fmt.Sprintf("%s:%s@tcp(%s:%s)/", os.Getenv("AVHBS_DB_USER"), os.Getenv("AVHBS_DB_PASS"), os.Getenv("AVHBS_DB_IP"), os.Getenv("AVHBS_DB_PORT")) + os.Getenv("AVHBS_DB_NAME") + "?parseTime=true"
 	fmt.Println("Database Login Info:")
 	fmt.Println(loginInfo)
-	//loginInfo = loginInfo + os.Getenv("AVHBS_DB_NAME") + "?parseTime=true"
 	db, _ = sql.Open("mysql", loginInfo)
 
 	insertClients()
@@ -22,7 +21,7 @@ func main() {
 	insertUsers()
 	insertItems()
 	insertBookings()
-	insertFavoriteItems()
+	insertPayments()
 	insertFeedback()
 }
 
@@ -32,52 +31,45 @@ func insertClients() {
 }
 
 func insertPasswords() {
-	db.Exec("INSERT INTO passwords VALUES('secret', NOW() - INTERVAL 6 DAY);")
+	db.Exec("INSERT INTO passwords VALUES('secret', NOW() - INTERVAL 6 DAY, 'Carmerstraße');")
 }
 
 func insertUsers() {
-	db.Exec("INSERT INTO users VALUES(0, NOW() - INTERVAL 6 DAY, 'Alpha', 'Ahmed', 'Angus', '', 'Aktiv B', '', '', -19, -50);")
-	db.Exec("INSERT INTO users VALUES(0, NOW() - INTERVAL 6 DAY + INTERVAL 2 MINUTE, 'Beta', 'Bert', '', '', 'Aktiv B', '', '', -8.05, -50);")
-	db.Exec("INSERT INTO users VALUES(0, NOW() - INTERVAL 6 DAY + INTERVAL 4 MINUTE, 'Gamma', '', 'Gimel', '', 'AH', '', '', 0, -100);")
-	db.Exec("INSERT INTO users VALUES(0, NOW() - INTERVAL 6 DAY + INTERVAL 7 MINUTE, '', 'Delta', 'Dalet', '', 'Gast', 'delta@dalet.com', '+49 123 456', 20, -50);")
-	db.Exec("INSERT INTO users VALUES(0, NOW() - INTERVAL 6 DAY + INTERVAL 8 MINUTE, '', 'Epsilon', 'Epikur', '', 'Gast', 'epikur@athens.gr', '', -110, -50);")
-	db.Exec("INSERT INTO users VALUES(0, NOW() - INTERVAL 5 DAY, 'Zeta', '', '', '', 'AH', '', '', 0, -100);")
-	db.Exec("INSERT INTO users VALUES(0, NOW() - INTERVAL 4 DAY, '', 'Emily', 'Everest', 'Eta', 'Steganleger', 'Sagarmatha@web.de', '', 0, -50);")
-	db.Exec("INSERT INTO users VALUES(0, NOW() - INTERVAL 4 DAY + INTERVAL 4 MINUTE, '', 'Timothy', 'Tiger', 'Theta', 'Steganleger', '', '+41 456 789', 0, -50);")
+	db.Exec("INSERT INTO users VALUES(0, NOW() - INTERVAL 6 DAY, 'Carmerstraße', 'Alpha', 'Ahmed', 'Angus', '', 'Aktiv B', '', '', -19, -50);")
+	db.Exec("INSERT INTO users VALUES(0, NOW() - INTERVAL 6 DAY + INTERVAL 2 MINUTE, 'Carmerstraße', 'Beta', 'Bert', '', '', 'Aktiv B', '', '', -8.05, -50);")
+	db.Exec("INSERT INTO users VALUES(0, NOW() - INTERVAL 6 DAY + INTERVAL 4 MINUTE, 'Carmerstraße', 'Gamma', '', 'Gimel', '', 'AH', '', '', 0, -100);")
+	db.Exec("INSERT INTO users VALUES(0, NOW() - INTERVAL 6 DAY + INTERVAL 7 MINUTE, 'Pichelütte', '', 'Delta', 'Dalet', '', 'Gast', 'delta@dalet.com', '+49 123 456', 20, -50);")
+	db.Exec("INSERT INTO users VALUES(0, NOW() - INTERVAL 6 DAY + INTERVAL 8 MINUTE, 'Pichelütte', '', 'Epsilon', 'Epikur', '', 'Gast', 'epikur@athens.gr', '', -110, -50);")
+	db.Exec("INSERT INTO users VALUES(0, NOW() - INTERVAL 5 DAY, 'Carmerstraße', 'Zeta', '', '', '', 'AH', '', '', 0, -100);")
+	db.Exec("INSERT INTO users VALUES(0, NOW() - INTERVAL 4 DAY, 'Pichelütte' '', 'Emily', 'Everest', 'Eta', 'Steganleger', 'Sagarmatha@web.de', '', 0, -50);")
+	db.Exec("INSERT INTO users VALUES(0, NOW() - INTERVAL 4 DAY + INTERVAL 4 MINUTE, 'Carmerstraße', '', 'Timothy', 'Tiger', 'Theta', 'Steganleger', '', '+41 456 789', 0, -50);")
 }
 
 func insertItems() {
-	db.Exec("INSERT INTO items VALUES(0, NOW() - INTERVAL 6 DAY, 'Agua', 'non-alcoholic', 1, 'l', 1);")
-	db.Exec("INSERT INTO items VALUES(0, NOW() - INTERVAL 6 DAY + INTERVAL 2 MINUTE, 'Beer', 'alcoholic', 0.5, 'l', 1.5);")
-	db.Exec("INSERT INTO items VALUES(0, NOW() - INTERVAL 6 DAY + INTERVAL 3 MINUTE, 'Beer', 'alcoholic', 0.3, 'l', 0.9);")
-	db.Exec("INSERT INTO items VALUES(0, NOW() - INTERVAL 6 DAY + INTERVAL 20 MINUTE, 'Cauliflower', 'food', 1, 'piece', 2.35);")
-	db.Exec("INSERT INTO items VALUES(0, NOW() - INTERVAL 6 DAY + INTERVAL 25 MINUTE, 'Date Juice', 'non-alcoholic', 0.5, 'l', 5);")
-	db.Exec("INSERT INTO items VALUES(0, NOW() - INTERVAL 5 DAY + INTERVAL 8 MINUTE, 'Eggnog', 'non-alcoholic', 0.2, 'l', 2);")
+	db.Exec("INSERT INTO items VALUES(0, NOW() - INTERVAL 6 DAY, 'Pichelütte', 'Agua', 'non-alcoholic', 1, 'l', 1);")
+	db.Exec("INSERT INTO items VALUES(0, NOW() - INTERVAL 6 DAY + INTERVAL 2 MINUTE, 'Carmerstraße', 'Beer', 'alcoholic', 0.5, 'l', 1.5);")
+	db.Exec("INSERT INTO items VALUES(0, NOW() - INTERVAL 6 DAY + INTERVAL 3 MINUTE, 'Carmerstraße', 'Beer', 'alcoholic', 0.3, 'l', 0.9);")
+	db.Exec("INSERT INTO items VALUES(0, NOW() - INTERVAL 6 DAY + INTERVAL 20 MINUTE, 'Carmerstraße', 'Cauliflower', 'food', 1, 'piece', 2.35);")
+	db.Exec("INSERT INTO items VALUES(0, NOW() - INTERVAL 6 DAY + INTERVAL 25 MINUTE, 'Pichelütte', 'Date Juice', 'non-alcoholic', 0.5, 'l', 5);")
+	db.Exec("INSERT INTO items VALUES(0, NOW() - INTERVAL 5 DAY + INTERVAL 8 MINUTE, 'Pichelütte', 'Eggnog', 'non-alcoholic', 0.2, 'l', 2);")
 }
 
 func insertBookings() {
-	db.Exec("INSERT INTO bookings VALUES(0, NOW() - INTERVAL 6 DAY + INTERVAL 2 MINUTE, 1, 1, 1, -1, 'Part 1/1', '');")
-	db.Exec("INSERT INTO bookings VALUES(0, NOW() - INTERVAL 5 DAY + INTERVAL 33 MINUTE, 2, 3, 2, -1.8, 'Part 1/1', '');")
-	db.Exec("INSERT INTO bookings VALUES(0, NOW() - INTERVAL 5 DAY + INTERVAL 90 MINUTE, 1, 2, 12, -18, 'Part 1/1', '');")
-	db.Exec("INSERT INTO bookings VALUES(0, NOW() - INTERVAL 4 DAY + INTERVAL 12 MINUTE, 3, 5, 2, -10, 'Part 1/2', '');")
-	db.Exec("INSERT INTO bookings VALUES(0, NOW() - INTERVAL 4 DAY + INTERVAL 12 MINUTE, 3, 6, 1, -2, 'Part 2/2', '');")
-	db.Exec("INSERT INTO bookings VALUES(0, NOW() - INTERVAL 4 DAY + INTERVAL 55 MINUTE, 3, 0, 1, 12, 'Payment Full', 'Cash');")
-	db.Exec("INSERT INTO bookings VALUES(0, NOW() - INTERVAL 2 DAY + INTERVAL 44 MINUTE, 4, 0, 1, 20, 'Payment Part', 'Cash');")
-	db.Exec("INSERT INTO bookings VALUES(0, NOW() - INTERVAL 1 DAY + INTERVAL 82 MINUTE, 5, 5, 22, -110, 'Part 1/1', '');")
-	db.Exec("INSERT INTO bookings VALUES(0, NOW() - INTERVAL 30 MINUTE, 2, 4, 3, -7.05, 'Part 1/1', '');")
+	db.Exec("INSERT INTO bookings VALUES(0, NOW() - INTERVAL 6 DAY + INTERVAL 2 MINUTE, 'Carmerstraße', 1, 1, 1, -1, 'Part 1/1');")
+	db.Exec("INSERT INTO bookings VALUES(0, NOW() - INTERVAL 5 DAY + INTERVAL 33 MINUTE, 'Carmerstraße', 2, 3, 2, -1.8, 'Part 1/1');")
+	db.Exec("INSERT INTO bookings VALUES(0, NOW() - INTERVAL 5 DAY + INTERVAL 90 MINUTE, 'Carmerstraße',  1, 2, 12, -18, 'Part 1/1');")
+	db.Exec("INSERT INTO bookings VALUES(0, NOW() - INTERVAL 4 DAY + INTERVAL 12 MINUTE, 'Pichelütte', 3, 5, 2, -10, 'Part 1/2');")
+	db.Exec("INSERT INTO bookings VALUES(0, NOW() - INTERVAL 4 DAY + INTERVAL 12 MINUTE, 'Pichelütte', 3, 6, 1, -2, 'Part 2/2');")
+	db.Exec("INSERT INTO bookings VALUES(0, NOW() - INTERVAL 1 DAY + INTERVAL 82 MINUTE, 'Pichelütte', 5, 5, 22, -110, 'Part 1/1');")
+	db.Exec("INSERT INTO bookings VALUES(0, NOW() - INTERVAL 30 MINUTE, 'Carmerstraße', 2, 4, 3, -7.05, 'Part 1/1');")
 }
 
-func insertFavoriteItems() {
-	db.Exec("INSERT INTO favorite_items VALUES(1, 1, 1);")
-	db.Exec("INSERT INTO favorite_items VALUES(2, 3, 2);")
-	db.Exec("INSERT INTO favorite_items VALUES(1, 2, 12);")
-	db.Exec("INSERT INTO favorite_items VALUES(3, 5, 2);")
-	db.Exec("INSERT INTO favorite_items VALUES(3, 6, 1);")
-	db.Exec("INSERT INTO favorite_items VALUES(5, 5, 22);")
-	db.Exec("INSERT INTO favorite_items VALUES(2, 4, 3);")
+func insertPayments() {
+	db.Exec("INSERT INTO payments VALUES(0, NOW() - INTERVAL 4 DAY + INTERVAL 55 MINUTE, 'Pichelütte', 3, 12, 'Cash', '');")
+	db.Exec("INSERT INTO payments VALUES(0, NOW() - INTERVAL 2 DAY + INTERVAL 44 MINUTE, 'Carmerstraße', 4, 20, 'Cash', '');")
 }
 
 func insertFeedback() {
-	db.Exec("INSERT INTO feedback VALUES(0, NOW() - INTERVAL 4 DAY + INTERVAL 2 MINUTE, 'AVHBS = AVH Bullshit. Lol.', 'Troll');")
-	db.Exec("INSERT INTO feedback VALUES(0, NOW() - INTERVAL 1 DAY + INTERVAL 100 MINUTE, 'Great work!', '');")
+	db.Exec("INSERT INTO feedback VALUES(0, NOW() - INTERVAL 4 DAY + INTERVAL 2 MINUTE, 'Pichelütte', 'AVHBS = AVH Bullshit. Lol.', 'Troll');")
+	db.Exec("INSERT INTO feedback VALUES(0, NOW() - INTERVAL 1 DAY + INTERVAL 100 MINUTE, 'Carmerstraße', 'Great work!', '');")
 }
