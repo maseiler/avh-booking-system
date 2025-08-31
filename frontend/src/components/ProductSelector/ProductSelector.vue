68457d411226857867f5f3131b5d9d49a48b6c3e
ebert.l <info@leon-ebert.de>
2025-08-31 16:27:45 +0200

    Added Category Icons

--- frontend/src/components/ProductSelector/ProductSelector.vue
+++ frontend/src/components/ProductSelector/ProductSelector.vue
@@ -11,7 +11,10 @@
   <!-- Category Filter -->
   <div class="tags is-flex is-justify-content-space-around category-filter">
     <button class="tag is-hoverable" :class="selectedCategory == 0 ? 'is-primary' : ''" @click="selectCategory(0)">All</button>
-    <button v-for="category in categorys" class="tag is-hoverable" :class="selectedCategory == category.id ? 'is-primary' : ''" @click="selectCategory(category.id)">{{ category.title }}</button>
+    <button v-for="category in categorys" class="tag is-hoverable" :class="selectedCategory == category.id ? 'is-primary' : ''" @click="selectCategory(category.id)">
+      <span class="icon"><icon :icon="category.icon"/></span>
+      <span>{{ category.title }}</span>
+    </button>
   </div>
   <hr>
 
