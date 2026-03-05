b9a52249c51ca4102b84c386cb2224f88e512364
ebert-l <info@leon-ebert.de>
2026-03-05 19:21:52 +0100

    Changed Icons

--- frontend/src/views/Settings/AccountSettingsSingle.vue
+++ frontend/src/views/Settings/AccountSettingsSingle.vue
@@ -125,11 +125,11 @@
     <div class="column is-3"></div>
     <div class="column">
       <Buttons>
-        <Button :fa-icon="['fas', 'undo']" icon-position="left" @click="$router.go(-1)">
+        <Button :fa-icon="['fas', 'times']" icon-position="left" @click="$router.go(-1)">
           Cancel
         </Button>
 
-        <Button class="is-primary" @click="actionButtonClicked" :fa-icon="['fas', 'cloud-upload']" icon-position="right">
+        <Button class="is-primary" @click="actionButtonClicked" :fa-icon="['fas', 'save']" icon-position="right">
           {{ actionButton }}
         </Button>
       </Buttons>      
