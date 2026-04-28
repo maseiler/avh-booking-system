6e1b6b443931b2ba08562b0ab12b69d8b1101ccd
ebert-l <info@leon-ebert.de>
2026-04-28 22:01:33 +0200

    Added Setting Store and implemented Company Name and Company Slogan as examples to show how these settings will be retreived and edited in the future The Same procedure can be used for Logos, Language and Currency Settings.

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,14 +1,50 @@
 <script setup lang="ts">
-import HelloWorld from '../components/HelloWorld.vue';
+import { computed } from 'vue';
+import { useSettingStore } from '../store/SettingStore';
+
+const setting$ = useSettingStore();
+const compTitle = computed(() => {
+  let cT = setting$.get("compTitle");
+  if (cT == -1){
+    return "Set a Company Title in the Admin Settings"
+  }
+  return cT.value;
+});
+
+const compSlogan = computed(() => {
+  let cT = setting$.get("compSlogan");
+  if (cT == -1){
+    return "Set a Slogan in the Admin Settings"
+  }
+  return cT.value;
+})
+
+
+
+
 </script>
 
 <template>
-  <div class="box">
-    This is the Home Screen of the App
+  <div class="grid">
+    <div class="cell">
+      <h1 class="title">{{compTitle}}</h1>
+    </div>
+    <div class="cell">
+      <img src="./" alt="">
+    </div>
+    <div class="cell">
+      <span class="title is-4">{{compSlogan}}</span>
+    </div>
+
   </div>
-  <HelloWorld msg="Test Msg"/>
+  
 </template>
 
 <style scoped>
-
+.grid{
+  align-items:center;
+  justify-items: center;
+  grid-template-columns: auto;
+  grid-template-rows: 1fr auto 1fr;
+}
 </style>
---
+++ frontend/src/views/Settings/ApplicationSettings.vue
@@ -0,0 +1,194 @@
+<script lang="ts" setup>
+import { ref } from 'vue';
+import DetailsArea from '../../components/DetailsArea.vue';
+import { useSettingStore } from '../../store/SettingStore';
+import { computed } from 'vue';
+
+const setting$ = useSettingStore();
+
+const companyName =  computed( {
+  get() {
+    return setting$.get("compTitle").value;
+  },
+  set(newValue: string) {
+    setting$.set("compTitle", newValue);
+  }
+});
+
+const companySlogan =  computed( {
+  get() {
+    return setting$.get("compSlogan").value;
+  },
+  set(newValue: string) {
+    setting$.set("compSlogan", newValue);
+  }
+});
+
+const companyLogo = ref("");
+function changeLogo(e){
+  const reader = new FileReader();
+  reader.addEventListener("load", () => {
+    companyLogo.value = reader.result;
+  })
+
+  reader.readAsDataURL(e.target.files[0]);
+}
+
+const companyIcon = ref("");
+function changeIcon(e){
+  const reader = new FileReader();
+  reader.addEventListener("load", () => {
+    companyIcon.value = reader.result;
+    console.log(companyIcon.value.length);
+  })
+
+  reader.readAsDataURL(e.target.files[0]);
+}
+</script>
+
+<template>
+
+<h1 class="title">General Settings</h1>
+
+<DetailsArea>
+  <template #summary>
+    <h2 class="title is-4">Company Settings</h2>
+  </template>
+
+  <div class="columns">
+    <div class="column is-3">
+      Firmenname
+    </div>
+    <div class="column">
+      <p class="control has-icons-left">
+          <input type="text" v-model="companyName" class="input" placeholder="SOS Children's Villages">
+          <span class="icon is-small is-left">
+            <icon :icon="['fas', 'id-card']" />
+          </span>
+        </p>
+    </div>
+  </div>
+
+  <div class="columns">
+    <div class="column is-3">
+      Slogan
+    </div>
+    <div class="column">
+      <p class="control has-icons-left">
+          <input type="text" v-model="companySlogan" class="input" placeholder="Every child a home!">
+          <span class="icon is-small is-left">
+            <icon :icon="['fas', 'microphone']" />
+          </span>
+        </p>
+    </div>
+  </div>
+
+  <div class="columns">
+    <div class="column is-3">
+      Logo (.png below 100kb)
+    </div>
+    <div class="column">
+      <p class="control has-icons-left">
+        <input type="file" class="input" accept="image/png, .svg" @change="changeLogo">
+        <span class="icon is-small is-left">
+          <icon :icon="['fas', 'image']" />
+        </span>
+      </p>
+      <!-- Logo Preview -->
+      <img width="150px" :src="companyLogo.valueOf()">
+
+    </div>
+  </div>
+
+  <div class="columns">
+    <div class="column is-3">
+      Icon
+    </div>
+    <div class="column">
+      <p class="control has-icons-left">
+        <input type="file" class="input" accept="image/png, .svg" @change="changeIcon">
+        <span class="icon is-small is-left">
+          <icon :icon="['fas', 'image']" />
+        </span>
+      </p>
+      <!-- Icon Preview -->
+      <img width="50px ":src="companyIcon.valueOf()">
+    </div>
+  </div>
+
+</DetailsArea>
+
+<DetailsArea>
+  <template #summary>
+    <h2 class="title is-4">Application Look</h2>
+  </template>
+
+  <div class="columns">
+    <div class="column is-3">
+      Application Color Scheme (Dark)
+    </div>
+    <div class="column">
+      <div class="select">
+        <select>
+          <option value="">Not yet implemented</option>
+        </select>
+      </div>
+    </div>
+  </div>
+
+  <div class="columns">
+    <div class="column is-3">
+      Application Color Scheme (Light)
+    </div>
+    <div class="column">
+      <div class="select">
+        <select>
+          <option value="">Not yet implemented</option>
+        </select>
+      </div>
+    </div>
+  </div>
+
+  <div class="columns">
+    <div class="column is-3">
+      Primary Color
+    </div>
+    <div class="column is-2">
+      <input type="color" class="input">
+    </div>
+  </div>
+
+</DetailsArea>
+
+<DetailsArea>
+  <template #summary>
+    <h2 class="title is-4">Localisation</h2>
+  </template>
+
+  <div class="columns">
+    <div class="column is-3">
+      {{ $t('settings.language')}}
+    </div>
+    <div class="column is-2">
+      <div class="select">
+        <select v-model="$i18n.locale">
+          <option v-for="l of $i18n.availableLocales" :key="`locale-${l}`" :value="l">{{ $t(`settings.${l}`) }}</option>
+        </select>
+      </div>
+    </div>
+  </div>
+
+  <!-- ToDo Allow seperate changing of currency -->
+
+
+</DetailsArea>
+
+
+
+</template>
+
+<style scoped>
+.columns{
+  align-items: center;
+}
+</style>
