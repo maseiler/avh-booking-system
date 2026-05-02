21cba5fb05a12fd543efe7075748887f9d5b5bf3
ebert-l <info@leon-ebert.de>
2026-05-02 11:15:49 +0200

    Ideas to improve the Client Setup routine

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,4 +1,8 @@
 <template>
+  <!-- ToDo: Maybe a whole 'Welcome Experience' should be introduced.
+   It could just be limited to setting the ClientId, when evrything else is done.
+   But if this is the first installation, maybe it would be nice to be able to be redirected to the
+   Admin Settings directly. -->
   <ClientSetup v-if="needsSetup" @setup-complete="onSetupComplete" />
   <template v-else>
     <DevModeBar v-if="dev" />
@@ -20,6 +24,8 @@ export default {
     ClientSetup,
   },
   data() {
+    // ToDo: move this to a computed value OR
+    // Move this functionality 'needsSetup()'/'setupComplete()' to the SettingStore and let it handle it.
     const clientId = localStorage.getItem('avhbs_client_id');
     return {
       dev: false,
@@ -34,7 +40,13 @@ export default {
     }
   },
   methods: {
+    // ToDo: either remove this function or use it properly when the ClientSetup actually returns an event.
+    // currently the Component doenst emit this Event and the functino will not be called at all.
+
+    //ToDo: Remove this function and move it into SettingStore so the Components and Views try to do as much as possible on their own. 
     onSetupComplete() {
+      // At the Top is is asked if 'needsSetup' is true. Why not simply setting this value to false?
+      // A reload of the page should never be the solution to fix broken Data/Behaviour.
       location.reload();
     },
   },
